3b14ce714966bb2e7cfc7c1c5e56ab1b82c23b81
drnobl <iddrisusharifdeen@gmail.com>
2023-03-16 13:35:10 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * 
- * main - This is the main fucntion to print string using puts
+ * main - Entry point for Hello program
  *
- * Return: The return is executed when the program is completed
- *
+ * Return: Return is always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
