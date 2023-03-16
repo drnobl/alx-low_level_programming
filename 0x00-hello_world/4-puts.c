94bb155ff82f1de1e3a045ade77111ef43d17709
drnobl <iddrisusharifdeen@gmail.com>
2023-03-16 13:04:35 -0700

    Hello in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * 
+ * main - This is the main fucntion to print string using puts
+ *
+ * Return: The return is executed when the program is completed
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
