fd674625d39eb7c4803812bf70c67d9b13d534c8
drnobl <iddrisusharifdeen@gmail.com>
2023-03-16 16:11:32 -0700

    Yess

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 /**
  * main - Entry point for Hello program
@@ -9,5 +8,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }
