7a0d3271994731d2b698dcd70872d194a2ee885a
drnobl <iddrisusharifdeen@gmail.com>
2023-03-16 15:39:16 -0700

    TAsk8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point for Hello program
+ *
+ * Return: Return 1 on  (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
+	return(1);
+}
