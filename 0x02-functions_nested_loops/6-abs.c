ff276196d029007eba83bff6691c3055a735f83b
drnobl <iddrisusharifdeen@gmail.com>
2023-03-21 15:02:39 -0700

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ *@ab: Int ab prints the abolute value
+ * Return: Always 0.
+ */
+int _abs(int ab)
+{
+	if (ab >= 0)
+	{
+		return (ab);
+	}
+	return (-ab);
+}
