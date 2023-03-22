da2609a36f17318168908be554cb85f1d65a688a
drnobl <iddrisusharifdeen@gmail.com>
2023-03-21 21:45:49 -0700

    Second submit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * add - A function that adds two integers and returns the result.
- * @x: Parametr 1
- * @y: Paramter2
- * @res: Parameter results
- *
- * Return: Always 0.
- */
-int add(int x, int y)
-{
-	int res = x + y;
 
-	return (res);
-	_putchar('\n');
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a this are the parameters
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
 }
