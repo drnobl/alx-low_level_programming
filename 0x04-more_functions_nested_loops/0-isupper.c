dfd51ba3b7528504498f8256c19d38905bc3f30a
drnobl <iddrisusharifdeen@gmail.com>
2023-03-23 11:07:12 -0700

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * int _isupper(int c) - Funtion for the Program
+ *
+ *
+ * @c: Integer parameter
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
