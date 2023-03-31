47580cdad4cd7c931ff7888b2b1397f466642eb0
drnobl <iddrisusharifdeen@gmail.com>
2023-03-30 19:57:29 -0700

    task11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * *infinite_add - A function to add two integers
+ * @n1: Integer Parameter 1
+ * @n2: Integer parameter 2
+ * @r: the buffer that the function will use to store the result
+ * @size_r:  is the buffer size
+ *
+ * Return: Awayl 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+{
+	r = (n1 + n2)
+		return (r)
+}
