e9ec0a718e6d48e3bdea35dcb1780e222e208331
drnobl <iddrisusharifdeen@gmail.com>
2023-04-12 09:16:31 -0700

    Static library_task0

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -10,9 +10,9 @@
 
 int _isalpha(int c)
 {
-        if (c >= 'A' && c <= 'Z')||(c >= 'a' && c <= 'z')
+        if ((c >= 'A' && c <= 'Z')||(c >= 'a' && c <= 'z'))
 			{
-                return (1);
-        }
-        return (0);
-} 
+				return (1);
+			}
+	return (0);
+}
