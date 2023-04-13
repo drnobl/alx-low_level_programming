d60bd8e73097791b61cf100325c887598290df83
drnobl <iddrisusharifdeen@gmail.com>
2023-04-13 09:41:23 -0700

    Task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ *create_array - Entry point for the program
+ *
+ * @c: Char Parameter
+ * Return: 0 on success
+ */
+char *create_array(unsigned int size, char c)
+		
+{
+	
+	char *str;		
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+		
+		return (NULL);		
+
+
+	for (i = 0; i < size; i++)
+char *create_array(unsigned int size, char c)
+		
+{
+		
+	char *str;
+		
+	unsigned int i;
+		
+
+		
+	str = malloc(sizeof(char) * size);
+		
+	if (size == 0)/* ||str==NULL*/		
+		return (NULL);
+		
+
+		
+	for (i = 0; i < size; i++)
+		
+		str[i] = c;
+		
+	return (str);
+}
