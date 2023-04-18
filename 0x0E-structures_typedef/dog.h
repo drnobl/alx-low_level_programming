9a5b981bf365c50afc62a97ac1c09c054f702d14
drnobl <iddrisusharifdeen@gmail.com>
2023-04-17 17:46:25 -0700

    Task1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Struct of the function
+ * @name: Name of the dog
+ * @age: of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+char *name;
+	float age;
+char *owner;
+}
+#endif
