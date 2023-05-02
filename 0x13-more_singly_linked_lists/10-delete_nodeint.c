a177b6c12092415075fad045b54c6bdb43cd3618
drnobl <iddrisusharifdeen@gmail.com>
2023-05-02 09:05:45 -0700

    Delete Node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (idx < index - 1)
 	{
-		if !(temp->next) || (!temp)
+		if (!(temp->next) || (!temp))
 
 			return (-1);
 		temp = temp->next;
